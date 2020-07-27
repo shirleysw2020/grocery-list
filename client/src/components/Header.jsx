3c0a390b8624b597f8fcd5688ca257a65d17fa61
Maia Ling <electric.pen@gmail.com>
2020-07-27 12:01:17 -0700

    Working client code with Form and List

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <img src="grocery-bags.png" />
+    <h1>Grocery List</h1>
+  </div>
+)
+
+export default Header;
---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// ReactDOM.render has two arguments:
+  // the react component to attatch to the DOM (root component)
+  // the position in the html to attatch the component to
